bb26dc467ba6cf74f950197950776e49c62df6e3
tobi2018 <viettt.uit@gmail.com>
2021-11-09 11:59:28 +0700

    test
    new hello world

--- ExampleCloudScript.js.js
+++ ExampleCloudScript.js.js
@@ -4,11 +4,18 @@
 var TEST_TITLE_ID = "6195"; // NOTE: Replace this with your own titleID - DeleteUsers has an additional security check to avoid accidents
 var TEST_DATA_KEY = "TEST_DATA_KEY"; // Used to reuse args.customId, but it was kindof a pain, and made things fragile
 
-var HelloWorld = function (args, context) {
-    return "HelloWorld";
+handlers.helloWorld = function(args, content) {
+    let msg = "Tobi, Hello world";
+    return msg;
 };
-handlers.helloWorld = HelloWorld;
 
 var calculator = function(a, b){
     return a + b;
+}
+
+var funcTest = function(args, content){
+    const a = 1;
+    if(true){
+        let a = 2;
+    }
 }
